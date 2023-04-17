2bd5624ffb365d09a0bf0462bd909f89aa0d4624
Praveen Sanikommu <praveenreddy206@gmail.com>
2023-04-17 05:42:42 +0530

    Adding Bundler

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
     const heading1 = React.createElement('h1', {
         id: 'title',
         className: 'test1',
-    }, 'Welcome to React!');
+    }, 'Welcome to React from Parcel!');
     const heading2 = React.createElement('h2', {
         id: 'title',
         className: 'test2'
-    }, 'Namasthe React!');
+    }, 'Namasthe React from Parcel!');
     const container = React.createElement('div', {
         id: 'container',
     }, [heading1, heading2])
